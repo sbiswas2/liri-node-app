c4d7ef142098933c66e9948f12dd33c1d27f9d12
Setu Biswas <setubiswas@Setus-MacBook-Pro.local>
2017-10-07 15:41:17 -0500

    initial set up

---
+++ keys.js
@@ -0,0 +1,10 @@
+console.log('this is loaded');
+
+var twitterKeys = {
+  consumer_key: 'mL6CubL42S1n6GMnGgCM8aHIY',
+  consumer_secret: '4h9sPc72fNzG12bvf1qUsySe3Pnxc5jGrwsPTHrUmVWRjPUcZH',
+  access_token_key: '916754027702050817-Yi3wBu5ujDJHTCL7bIqSXKSRUGX4ZCr',
+  access_token_secret: 'C8U58ZJ4MXcYoCzGdR9XFSO10a49mFODDyNioIRKxLmPH',
+}
+
+module.exports = twitterKeys;
