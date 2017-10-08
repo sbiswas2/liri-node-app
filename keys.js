460f348d45a0a70de69fc8e22ef8f26d7d370009
Setu Biswas <setubiswas@setus-mbp.attlocal.net>
2017-10-08 12:37:56 -0500

    fixed errors

--- keys.js
+++ keys.js
@@ -5,15 +5,14 @@ var twitterKeys = {
   consumer_secret: '4h9sPc72fNzG12bvf1qUsySe3Pnxc5jGrwsPTHrUmVWRjPUcZH',
   access_token_key: '916754027702050817-Yi3wBu5ujDJHTCL7bIqSXKSRUGX4ZCr',
   access_token_secret: 'C8U58ZJ4MXcYoCzGdR9XFSO10a49mFODDyNioIRKxLmPH',
-}
+};
 
 var spotifyKeys = {
 	id: 'af606e4bbe09447db7752de7d9902a62',
 	secret: 'c4496d7453d94ef99894c9868428b73d',
-}
+};
 
 module.exports = {
 	twitterKeys,
 	spotifyKeys
-}
-// module.exports = spotifyKeys;
+};
