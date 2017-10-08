804499c94d2917b497e6dfd42e4b08d6dd2f69e5
Setu Biswas <setubiswas@setus-mbp.attlocal.net>
2017-10-08 15:53:51 -0500

    adjust tomato ratings

--- liri.js
+++ liri.js
@@ -62,11 +62,11 @@ function findMovie() {
 		console.log(error);
 		return;
 	};
-	
+
     console.log("Title: " + JSON.parse(body).Title);
     console.log("Year: " + JSON.parse(body).Year);
     console.log("IMDb Rating: " + JSON.parse(body).imdbRating);
-    console.log("Rotten Tomatoes Rating: " + JSON.parse(body).tomatoRating);
+    console.log("Rotten Tomatoes Rating: " + JSON.parse(body).Ratings[1].Value);
     console.log("Country: " + JSON.parse(body).Country);
     console.log("Language: " + JSON.parse(body).Language);
     console.log("Plot: " + JSON.parse(body).Plot);
